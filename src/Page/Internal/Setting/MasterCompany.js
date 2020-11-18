c6bb8dcab28a63d0909db02aa5b1ff69509a7dd1
worasalid <worasalid@iconframework.com>
2020-11-18 17:11:16 +0700

    สร้างหน้า Master Company

--- src/Page/Internal/Setting/MasterCompany.js
+++ src/Page/Internal/Setting/MasterCompany.js
@@ -1,6 +1,6 @@
 import { EditOutlined } from '@ant-design/icons';
-import { Button, Table, Input, Form } from 'antd';
-import Modal from 'antd/lib/modal/Modal';
+import { Button, Table, Input, InputNumber, Form, Modal } from 'antd';
+
 import Column from 'antd/lib/table/Column';
 import Axios from 'axios'
 import React, { useEffect, useState } from 'react'
@@ -14,31 +14,112 @@ export default function MasterCompany() {
 
     //data
     const [listcompany, setListcompany] = useState([]);
+    const [selectcompany, setSelectcompany] = useState(null);
+    const [companyid, setCompanyid] = useState(null);
 
-    const GetCompany = async () => {
+    const GetCompany = async (value) => {
+        console.log("value", value)
         try {
-            const company = await Axios({
+            const company_all = await Axios({
                 url: process.env.REACT_APP_API_URL + "/master/company",
                 method: "GET",
                 headers: {
                     "Authorization": "Bearer " + localStorage.getItem("sp-ssid")
                 }
             });
-            setListcompany(company.data)
+            if (company_all.status === 200) {
+                setListcompany(company_all.data)
+            }
+
+            const companyby_id = await Axios({
+                url: process.env.REACT_APP_API_URL + "/master/company",
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem("sp-ssid")
+                },
+                params: {
+                    id: value
+                }
+            });
+
+            if (companyby_id.status === 200) {
+                setSelectcompany(companyby_id.data)
+            }
+
+
         } catch (error) {
 
         }
     }
 
-    const onFinish = values => {
+    const GetCompanyby_id = async (value) => {
+        console.log("value", value)
+        try {
+            const companyby_id = await Axios({
+                url: process.env.REACT_APP_API_URL + "/master/company",
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem("sp-ssid")
+                },
+                params: {
+                    id: value
+                }
+            });
+
+            if (companyby_id.status === 200) {
+                setSelectcompany(companyby_id.data);
+            }
+
+
+        } catch (error) {
+
+        }
+    }
+
+
+
+    const onFinish = async (values) => {
         console.log(values);
+        try {
+            const updatecompany = await Axios({
+                url: process.env.REACT_APP_API_URL + "/master/company-update",
+                method: "PATCH",
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem("sp-ssid")
+                },
+                data: {
+                    id: selectcompany[0]?.Id,
+                    name: values.name,
+                    full_name_th: values.fullname_th,
+                    full_name_en: values.fullname_en,
+                    cost: values.cost
+                }
+            });
+            if (updatecompany.status === 200) {
+                GetCompany()
+                Modal.info({
+                    title: 'บันทึกข้อมูลเรียบร้อย',
+                    content: (
+                        <div>
+                            <p></p>
+                        </div>
+                    ),
+                    onOk() {
+                        setVisible(false)
+                    },
+                });
+            }
+
+        } catch (error) {
+
+        }
     };
 
     useEffect(() => {
         GetCompany()
     }, [])
 
-    console.log("listcompany", listcompany)
+
     return (
         <MasterPage>
             <Table dataSource={listcompany} loading={false}>
@@ -55,6 +136,14 @@ export default function MasterCompany() {
                                     icon={<EditOutlined />}
                                     onClick={() => {
                                         return (
+                                            GetCompany(record.Id),
+                                            form.setFieldsValue({
+                                                code: record.Code,
+                                                name: record.Name,
+                                                fullname_th: record.FullNameTH,
+                                                fullname_en: record.FullNameEN,
+                                                cost: record.CostManday
+                                            }),
                                             setVisible(true)
                                         )
                                     }
@@ -70,23 +159,41 @@ export default function MasterCompany() {
             </Table>
 
             <Modal
-                title="ข้อมูลบริษัท"
+                title={`ข้อมูลบริษัท - ${selectcompany && selectcompany[0]?.Name}`}
                 visible={visible}
                 width={800}
-                onOk={() => setVisible(false)}
-                onCancel={() => setVisible(false)}
+                onOk={() => {
+                    form.submit();
+                    setVisible(false);
+                }
+                }
+                okButtonProps={{ type: "primary", htmlType: "submit" }}
+                onCancel={() => { setVisible(false); form.resetFields(); setSelectcompany(null) }}
             >
                 <Form form={form}
                     layout="horizontal"
                     name="form-company"
                     onFinish={onFinish}
+
                 >
+                  
                     <Form.Item name="code" label="code">
-                        <Input />
+                        <Input disabled={true} />
                     </Form.Item>
 
                     <Form.Item name="name" label="name">
                         <Input />
+                    </Form.Item>
+                    <Form.Item name="fullname_th" label="FullName TH">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="fullname_en" label="FullName EN">
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item name="cost" label="Cost manday">
+                        <InputNumber
+                            formatter={value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                        />
                     </Form.Item>
                 </Form>
 
